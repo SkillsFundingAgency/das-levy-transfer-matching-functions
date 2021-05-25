68d150a4a97f2eb75451e7f0c2c541a9a46e97fe
dannygb <dan.beavon@education.gov.uk>
2021-05-25 09:15:13 +0100

    Adds initial setup for funcitons app

---
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/QueueNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.LevyTransferMatching.Infrastructure
+{
+    public static class QueueNames
+    {
+        public const string CreatedAccount = "SFA.DAS.LevyTransferMatching.CreatedAccount";
+        public const string ChangedAccountName = "SFA.DAS.LevyTransferMatching.ChangedAccountName";
+        public const string RunHealthCheck = "SFA.DAS.LevyTransferMatching.MessageHandlers";
+    }
+}
