e0891fca8e22f54b5bf8cd3610f483eada2e8723
chrisfoster76 <chrisfoster186@googlemail.com>
2021-08-12 11:49:42 +0100

    Adds messages package Adds placeholder event handler

---
+++ src/SFA.DAS.LevyTransferMatching.Functions/Commands/ApplicationApprovedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.LevyTransferMatching.Infrastructure;
+using SFA.DAS.LevyTransferMatching.Messages.Events;
+using SFA.DAS.NServiceBus.AzureFunction.Attributes;
+
+namespace SFA.DAS.LevyTransferMatching.Functions.Commands
+{
+    public class ApplicationApprovedEventHandler
+    {
+        public ApplicationApprovedEventHandler()
+        {
+        }
+
+        [FunctionName("RunApplicationApprovedEvent")]
+        public async Task Run([NServiceBusTrigger(Endpoint = QueueNames.RunHealthCheck)] ApplicationApprovedEvent @event, ILogger log)
+        {
+            log.LogInformation($"Handling ApplicationApprovedEvent handler for application {@event.ApplicationId}");
+        }
+    }
+}
