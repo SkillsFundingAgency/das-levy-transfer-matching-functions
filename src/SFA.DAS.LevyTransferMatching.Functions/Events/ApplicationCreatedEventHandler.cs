98aa669cc63bdf42c6b4e0bbbb373501006c76da
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-03 10:20:54 +0000

    WIP

---
+++ src/SFA.DAS.LevyTransferMatching.Functions/Events/ApplicationCreatedEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using RestEase;
+using SFA.DAS.LevyTransferMatching.Functions.Api;
+using SFA.DAS.LevyTransferMatching.Infrastructure;
+using SFA.DAS.LevyTransferMatching.Infrastructure.Legacy;
+using SFA.DAS.LevyTransferMatching.Messages.Events;
+using SFA.DAS.NServiceBus.AzureFunction.Attributes;
+
+namespace SFA.DAS.LevyTransferMatching.Functions.Events
+{
+    public class ApplicationCreatedEventHandler
+    {
+        private readonly ILegacyTopicMessagePublisher _legacyTopicMessagePublisher;
+
+        public ApplicationCreatedEventHandler(ILegacyTopicMessagePublisher legacyTopicMessagePublisher)
+        {
+            _legacyTopicMessagePublisher = legacyTopicMessagePublisher;
+        }
+
+        [FunctionName("RunApplicationCreatedEvent")]
+        public async Task Run([NServiceBusTrigger(Endpoint = QueueNames.ApplicationCreated)] ApplicationCreatedEvent @event, ILogger log)
+        {
+            log.LogInformation($"Handling ApplicationCreated handler for application {@event.ApplicationId}");
+
+            try
+            {
+                _legacyTopicMessagePublisher.PublishAsync()
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, $"Error handling ApplicationCreatedEvent for application {@event.ApplicationId}");
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/Legacy/LegacyTopicMessagePublisher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Xml;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.LevyTransferMatching.Infrastructure.Legacy
+{
+    public class LegacyTopicMessagePublisher : ILegacyTopicMessagePublisher
+    {
+        private readonly ILogger<LegacyTopicMessagePublisher> _logger;
+        private readonly string _connectionString;
+
+        public LegacyTopicMessagePublisher(ILogger<LegacyTopicMessagePublisher> logger, string connectionString)
+        {
+            _logger = logger;
+            _connectionString = connectionString;
+        }
+
+        public async Task PublishAsync<T>(T @event)
+        {
+            var messageGroupName = GetMessageGroupName(@event);
+
+            ServiceBusClient client = null;
+            try
+            {
+                client = new ServiceBusClient(_connectionString);
+                var sender = client.CreateSender(messageGroupName);
+                var messageBody = Serialize(@event);
+                var message = new ServiceBusMessage(messageBody);
+                await sender.SendMessageAsync(message);
+
+                _logger.LogInformation($"Sent Message {typeof(T).Name} to Azure ServiceBus ");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error sending Message {typeof(T).Name} to Azure ServiceBus");
+                throw;
+            }
+            finally
+            {
+                if (client != null && !client.IsClosed)
+                {
+                    _logger.LogDebug("Closing legacy topic message publisher");
+                    await client.DisposeAsync();
+                }
+            }
+        }
+
+        private static string GetMessageGroupName(object obj)
+        {
+            CustomAttributeData customAttributeData = obj.GetType().CustomAttributes.FirstOrDefault<CustomAttributeData>((Func<CustomAttributeData, bool>)(att => att.AttributeType.Name == "MessageGroupAttribute"));
+            string str = customAttributeData != null ? (string)customAttributeData.ConstructorArguments.FirstOrDefault<CustomAttributeTypedArgument>().Value : (string)(object)null;
+            if (!string.IsNullOrEmpty(str))
+                return str;
+            return obj.GetType().Name;
+        }
+
+        private static byte[] Serialize<T>(T obj)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            var stream = new MemoryStream();
+            using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+            {
+                serializer.WriteObject(writer, obj);
+            }
+            return stream.ToArray();
+        }
+    }
+}
