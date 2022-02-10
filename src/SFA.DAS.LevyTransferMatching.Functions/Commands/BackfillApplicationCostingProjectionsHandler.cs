1683e02cec910e90fc1d4a10e909cd073e53ea15
chrisfoster76 <chrisfoster186@googlemail.com>
2022-02-10 09:40:29 +0000

    Initial implementation

---
+++ src/SFA.DAS.LevyTransferMatching.Functions/Commands/BackfillApplicationCostingProjectionsHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using RestEase;
+using SFA.DAS.LevyTransferMatching.Functions.Api;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Functions.Commands
+{
+    public class BackfillApplicationCostingProjectionsHandler
+    {
+        private readonly ILevyTransferMatchingApi _levyTransferMatchingApi;
+
+        public BackfillApplicationCostingProjectionsHandler(ILevyTransferMatchingApi levyTransferMatchingApi)
+        {
+            _levyTransferMatchingApi = levyTransferMatchingApi;
+        }
+
+        [FunctionName("BackfillApplicationCostingProjectionsHandler")]
+        public async Task Run([TimerTrigger("0 0 22 * * *")] TimerInfo timer, ILogger logger)
+        {
+            logger.LogInformation("Backfilling application costing projections");
+            
+            try
+            {
+                await _levyTransferMatchingApi.BackfillApplicationCostingProjections();
+            }
+            catch (ApiException ex)
+            {
+                logger.LogError(ex, $"Error backfilling costing projections");
+            }
+        }
+    }
+}
