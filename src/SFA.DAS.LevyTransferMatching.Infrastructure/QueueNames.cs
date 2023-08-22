d91f964607c5f529f32c1253a2f5ec262b44dc4a
Jon Harris <jon.harris@digital.education.gov.uk>
2023-08-22 07:57:49 +0100

    ApplicationCreatedForImmediateAutoApproval queue name exceeded 50 char limit

--- src/SFA.DAS.LevyTransferMatching.Infrastructure/QueueNames.cs
+++ src/SFA.DAS.LevyTransferMatching.Infrastructure/QueueNames.cs
@@ -11,7 +11,7 @@
         public const string TransferRequestApprovedEvent = "SFA.DAS.LTM.TransferRequestApproved";
         public const string ApplicationFundingDeclined = "SFA.DAS.LTM.ApplicationFundingDeclined";
         public const string ApplicationCreated = "SFA.DAS.LTM.ApplicationCreated";
-        public const string ApplicationCreatedForImmediateAutoApproval = "SFA.DAS.LTM.ApplicationCreatedForImmediateAutoApproval";
+        public const string ApplicationCreatedForImmediateAutoApproval = "SFA.DAS.LTM.ApplicationCreatedForImmdtAutoApproval";
         public const string ApplicationWithdrawn = "SFA.DAS.LTM.ApplicationWithdrawn";
         public const string ApplicationRejected = "SFA.DAS.LTM.ApplicationRejected";
         public const string ApplicationApproved = "SFA.DAS.LTM.ApplicationApproved";
