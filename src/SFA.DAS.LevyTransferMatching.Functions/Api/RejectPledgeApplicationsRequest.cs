d75f4ae99d488e8056c9a41f6116d2f9aad8ce15
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-28 15:51:51 +0100

    MAp-331 AutoClosePledge and RejectPledgeApplications functions

---
+++ src/SFA.DAS.LevyTransferMatching.Functions/Api/RejectPledgeApplicationsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.LevyTransferMatching.Functions.Api
+{
+    public  class RejectPledgeApplicationsRequest
+    {
+        public int PledgeId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.Functions/Commands/AutoClosePledgeCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using RestEase;
+using SFA.DAS.LevyTransferMatching.Functions.Api;
+using SFA.DAS.LevyTransferMatching.Infrastructure;
+using SFA.DAS.LevyTransferMatching.Messages.Commands;
+using SFA.DAS.LevyTransferMatching.Messages.Events;
+using SFA.DAS.NServiceBus.AzureFunction.Attributes;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Functions.Events
+{
+    public class AutoClosePledgeCommandHandler
+    {
+        private readonly ILevyTransferMatchingApi _api;
+
+        public AutoClosePledgeCommandHandler(ILevyTransferMatchingApi api)
+        {
+            _api = api;
+        }
+
+        [FunctionName("RunAutoClosePledgeCommand")]
+        public async Task Run([NServiceBusTrigger(Endpoint = QueueNames.AutoClosePledgeCommand)] AutoClosePledgeCommand @event, ILogger log)
+        {
+            log.LogInformation($"Handling AutoClosePledgeCommandHandler for pledge {@event.PledgeId}");
+
+            try
+            {
+                var request = new AutoClosePledgeRequest
+                {
+                    PledgeId = @event.PledgeId,
+                };
+                                
+                if (await _api.ClosePledge(request))
+                {
+                    await _api.RejectPledgeApplications(new RejectPledgeApplicationsRequest { PledgeId = @event.PledgeId });
+                }
+            }
+            catch (ApiException ex)
+            {
+                log.LogError(ex, $"Error handling AutoClosePledgeCommand");
+                throw;
+            }
+        }
+    }
+}
