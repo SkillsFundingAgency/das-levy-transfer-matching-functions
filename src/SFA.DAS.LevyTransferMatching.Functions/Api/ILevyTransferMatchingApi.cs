7f60fea031ce2b237a17a7a360b37bc480c03d10
Jon Harris <jon.harris@digital.education.gov.uk>
2024-01-17 14:05:21 +0000

    Merge

--- src/SFA.DAS.LevyTransferMatching.Functions/Api/ILevyTransferMatchingApi.cs
+++ src/SFA.DAS.LevyTransferMatching.Functions/Api/ILevyTransferMatchingApi.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.LevyTransferMatching.Functions.Api;
 public interface ILevyTransferMatchingApi
 {
     [Post("functions/application-approved")]
-    Task ApplicationApproved([Body]ApplicationApprovedRequest request);
+    Task ApplicationApproved([Body] ApplicationApprovedRequest request);
 
     [Post("functions/application-approved-receiver-notification")]
     Task ApplicationApprovedEmail([Body] ApplicationApprovedEmailRequest request);
@@ -16,14 +16,14 @@ public interface ILevyTransferMatchingApi
     [Post("functions/debit-application")]
     Task DebitApplication([Body] TransferRequestApprovedRequest request);
 
-        [Post("functions/application-funding-declined")]
-        Task ApplicationFundingDeclined([Body] ApplicationFundingDeclinedRequest request);  
-        
-        [Post("functions/reject-pledge-applications")]
-        Task RejectPledgeApplications([Body] RejectPledgeApplicationsRequest request);
+    [Post("functions/application-funding-declined")]
+    Task ApplicationFundingDeclined([Body] ApplicationFundingDeclinedRequest request);
 
-        [Get("functions/get-pending-application-email-data")]
-        Task<GetPendingApplicationEmailDataResponse> GetPendingApplicationEmailData();
+    [Post("functions/reject-pledge-applications")]
+    Task RejectPledgeApplications([Body] RejectPledgeApplicationsRequest request);
+
+    [Get("functions/get-pending-application-email-data")]
+    Task<GetPendingApplicationEmailDataResponse> GetPendingApplicationEmailData();
 
     [Post("functions/send-emails")]
     Task SendEmails([Body] SendEmailsRequest request);
