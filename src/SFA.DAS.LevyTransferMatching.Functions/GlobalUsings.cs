63544196ef3928167b3cb76d863b58d39b79fd87
Jon Harris <jon.harris@digital.education.gov.uk>
2024-02-02 15:50:01 +0000

    Tidy

--- src/SFA.DAS.LevyTransferMatching.Functions/GlobalUsings.cs
+++ src/SFA.DAS.LevyTransferMatching.Functions/GlobalUsings.cs
@@ -2,7 +2,6 @@ global using System;
 global using System.Collections.Generic;
 global using System.IO;
 global using System.Net;
-global using System.Text;
 global using System.Threading.Tasks;
 global using Microsoft.Azure.WebJobs;
 global using Microsoft.Extensions.Logging;
