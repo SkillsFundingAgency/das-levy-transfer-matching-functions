4468de87e886a0b341b6fca9b6c951d85e388154
Jon Harris <jon.harris@digital.education.gov.uk>
2024-04-16 10:27:40 +0100

    fix

--- src/SFA.DAS.LevyTransferMatching.Functions/Program.cs
+++ src/SFA.DAS.LevyTransferMatching.Functions/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -17,22 +18,21 @@ using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
 
 [assembly: NServiceBusTriggerFunction("SFA.DAS.LevyTransferMatching.Functions")]
 
+const string EndpointName = "SFA.DAS.LevyTransferMatching.Functions";
+const string ErrorEndpointName = $"{EndpointName}-error";
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureFunctionsWebApplication()
-    .ConfigureAppConfiguration((hostBuilderContext, builder) =>
-    {
-        builder.BuildDasConfiguration(hostBuilderContext.Configuration);
-    })
+    .ConfigureAppConfiguration((hostBuilderContext, builder) => { builder.BuildDasConfiguration(hostBuilderContext.Configuration); })
     .ConfigureServices((context, services) =>
     {
         services.AddDasLogging();
-        
+
         var serviceProvider = services.BuildServiceProvider();
 
         var configuration = context.Configuration;
-    // .BuildDasConfiguration();
+        // .BuildDasConfiguration();
 
         services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), configuration));
         services.AddOptions();
@@ -42,8 +42,8 @@ var host = new HostBuilder()
 
         var functionsConfig = configuration.GetSection(ConfigurationKeys.LevyTransferMatchingFunctions).Get<LevyTransferMatchingFunctions>();
 
-        var logger = serviceProvider.GetLogger(nameof(Program));
-        
+        // var logger = serviceProvider.GetLogger(nameof(Program));
+
         services.AddSingleton(configuration);
         //services.AddNServiceBus(functionsConfig, logger);
         services.AddLegacyServiceBus(functionsConfig);
@@ -52,12 +52,13 @@ var host = new HostBuilder()
 
         var apiConfig = configuration.GetSection(ConfigurationKeys.LevyTransferMatchingApi).Get<LevyTransferMatchingApiConfiguration>();
         var emailNotificationsConfig = configuration.GetSection(ConfigurationKeys.EmailNotifications).Get<EmailNotificationsConfiguration>();
-        
-        // logger.LogInformation("Host configuration dump: {Config}", JsonConvert.SerializeObject(configuration));
-        // logger.LogInformation("API configuration dump: {Config}", JsonConvert.SerializeObject(apiConfig));
-        // logger.LogInformation("Functions configuration dump: {Config}", JsonConvert.SerializeObject(functionsConfig));
 
-        Environment.SetEnvironmentVariable("AzureWebJobsServiceBus", functionsConfig.NServiceBusConnectionString);
+
+        // Might not need this, needs only AzureWebJobsServiceBus:fullyQualifiedNamespace env variable in azure to work with MI as per 
+        // https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/README.md#managed-identity-authentication
+
+        // MI isn't currently supported by NSB in isolation process
+        //Environment.SetEnvironmentVariable("AzureWebJobsServiceBus", functionsConfig.NServiceBusConnectionString);
         Environment.SetEnvironmentVariable("NSERVICEBUS_LICENSE", functionsConfig.NServiceBusLicense);
 
         services.Configure<EncodingConfig>(configuration.GetSection(ConfigurationKeys.EncodingService));
@@ -75,7 +76,7 @@ var host = new HostBuilder()
             .AddHttpMessageHandler<DefaultHeadersHandler>()
             .AddHttpMessageHandler<ApimHeadersHandler>()
             .AddHttpMessageHandler<LoggingMessageHandler>();
-        
+
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
@@ -83,15 +84,16 @@ var host = new HostBuilder()
     {
         // Function endpoints can create their own queues or other infrastructure in the Azure Service Bus namespace by using the configuration.AdvancedConfiguration.EnableInstallers() method.
         endpointConfiguration.AdvancedConfiguration.EnableInstallers();
+        endpointConfiguration.AdvancedConfiguration.SendFailedMessagesTo(ErrorEndpointName);
 
-        // var functionsConfig = hostConfig.GetSection(ConfigurationKeys.LevyTransferMatchingFunctions).Get<LevyTransferMatchingFunctions>();
-
+        // var functionsConfig = config.GetSection(ConfigurationKeys.LevyTransferMatchingFunctions).Get<LevyTransferMatchingFunctions>();
+        //
         // if (functionsConfig.NServiceBusConnectionString.Equals("UseDevelopmentStorage=true", StringComparison.CurrentCultureIgnoreCase))
         // {
-        //     endpointConfiguration
-        //         .UseTransport<LearningTransport>()
-        //         .StorageDirectory(Path.Combine(Directory.GetCurrentDirectory()[..Directory.GetCurrentDirectory().IndexOf("src", StringComparison.Ordinal)], @"src\.learningtransport")
-        //         );
+        //     // endpointConfiguration
+        //     //     .UseTransport<LearningTransport>()
+        //     //     .StorageDirectory(Path.Combine(Directory.GetCurrentDirectory()[..Directory.GetCurrentDirectory().IndexOf("src", StringComparison.Ordinal)], @"src\.learningtransport")
+        //     //     );
         // }
         // else
         // {
